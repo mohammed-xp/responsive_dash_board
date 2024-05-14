c8a340d6dbfd37a2422bab8c11c331e1326e46fc
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-05-14 07:11:33 +0200

    Add font family

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -4,70 +4,70 @@ abstract class AppStayles {
   static const TextStyle styleRegular16 = TextStyle(
     color: Color(0xff064061),
     fontSize: 16,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w400,
   );
 
   static const TextStyle styleBold16 = TextStyle(
     color: Color(0xff4EB7F2),
     fontSize: 16,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w700,
   );
 
   static const TextStyle styleMedium16 = TextStyle(
     color: Color(0xff064061),
     fontSize: 16,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w500,
   );
 
   static const TextStyle styleMedium20 = TextStyle(
     color: Color(0xffFFFFFF),
     fontSize: 20,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w500,
   );
 
   static const TextStyle styleSemiBold16 = TextStyle(
     color: Color(0xff064061),
     fontSize: 16,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w600,
   );
 
   static const TextStyle styleSemiBold20 = TextStyle(
     color: Color(0xff064061),
     fontSize: 20,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w600,
   );
 
   static const TextStyle styleRegular12 = TextStyle(
     color: Color(0xffAAAAAA),
     fontSize: 12,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w400,
   );
 
   static const TextStyle styleSemiBold24 = TextStyle(
     color: Color(0xff4EB7F2),
     fontSize: 24,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w600,
   );
 
   static const TextStyle styleRegular14 = TextStyle(
     color: Color(0xffAAAAAA),
     fontSize: 14,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w400,
   );
 
   static const TextStyle styleSemiBold18 = TextStyle(
     color: Color(0xff4EB7F2),
     fontSize: 18,
-    fontFamily: 'montserrat',
+    fontFamily: 'Montserrat',
     fontWeight: FontWeight.w600,
   );
 }
