1208f1c5f25af9a7ba7c0ba7b872b98af85b0f14
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-05-14 06:21:38 +0200

    Add Stayles

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStayles {
+  static const TextStyle styleRegular16 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 16,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleBold16 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontSize: 16,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle styleMedium16 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 16,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleMedium20 = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 20,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleSemiBold16 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 16,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleSemiBold20 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 20,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular12 = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontSize: 12,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleSemiBold24 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontSize: 24,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular14 = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontSize: 14,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleSemiBold18 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontSize: 18,
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w600,
+  );
+}
