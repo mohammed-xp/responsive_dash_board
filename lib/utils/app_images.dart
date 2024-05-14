f91a5ef768238455d45e03d3ae5a5690d7c53381
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-05-14 07:04:09 +0200

    Add assets and app iamges

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,56 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar_1.svg
+  static const String imagesAvatar1 = "assets/images/avatar_1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar_2.svg
+  static const String imagesAvatar2 = "assets/images/avatar_2.svg";
+
+  /// Assets for imagesBalanceIcon
+  /// assets/images/balance_icon.svg
+  static const String imagesBalanceIcon = "assets/images/balance_icon.svg";
+
+  /// Assets for imagesCardBackground
+  /// assets/images/card_background.svg
+  static const String imagesCardBackground = "assets/images/card_background.svg";
+
+  /// Assets for imagesDashboardIcon
+  /// assets/images/dashboard_icon.svg
+  static const String imagesDashboardIcon = "assets/images/dashboard_icon.svg";
+
+  /// Assets for imagesExpensesIcon
+  /// assets/images/expenses_icon.svg
+  static const String imagesExpensesIcon = "assets/images/expenses_icon.svg";
+
+  /// Assets for imagesIncomeIcon
+  /// assets/images/income_icon.svg
+  static const String imagesIncomeIcon = "assets/images/income_icon.svg";
+
+  /// Assets for imagesLogoutIcon
+  /// assets/images/logout_icon.svg
+  static const String imagesLogoutIcon = "assets/images/logout_icon.svg";
+
+  /// Assets for imagesMyInvestmentsIcon
+  /// assets/images/my_investments_icon.svg
+  static const String imagesMyInvestmentsIcon = "assets/images/my_investments_icon.svg";
+
+  /// Assets for imagesMyTransactionIcon
+  /// assets/images/my_transaction_icon.svg
+  static const String imagesMyTransactionIcon = "assets/images/my_transaction_icon.svg";
+
+  /// Assets for imagesSettingIcon
+  /// assets/images/setting_icon.svg
+  static const String imagesSettingIcon = "assets/images/setting_icon.svg";
+
+  /// Assets for imagesStatisticsIcon
+  /// assets/images/statistics_icon.svg
+  static const String imagesStatisticsIcon = "assets/images/statistics_icon.svg";
+
+  /// Assets for imagesWalletAccountIcon
+  /// assets/images/wallet_account_icon.svg
+  static const String imagesWalletAccountIcon = "assets/images/wallet_account_icon.svg";
+}
+
