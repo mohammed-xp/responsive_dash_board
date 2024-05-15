60f4dffe03dd5d471a47e6c89d08c7d25af50ade
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-05-15 05:49:44 +0200

    remove import

--- lib/widgets/drawer_item.dart
+++ lib/widgets/drawer_item.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_dash_board/models/drawer_item_model.dart';
-import 'package:responsive_dash_board/utils/app_styles.dart';
 
 import 'active_and_inactive_item.dart';
 
