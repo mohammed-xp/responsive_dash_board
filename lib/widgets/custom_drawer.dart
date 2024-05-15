d5f3b19d1aee80e0808fd7210eb490e6deccd209
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-05-15 05:50:19 +0200

    Finish custom drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/active_and_inactive_item.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 import 'drawer_items_list_view.dart';
@@ -11,14 +13,52 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
-        children: [
-          UserInfoListTile(
-            image: Assets.imagesAvatar3,
-            title: 'Lekan Okeowo',
-            subtitle: 'demo@gmail.com',
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListTile(
+              image: Assets.imagesAvatar3,
+              title: 'Lekan Okeowo',
+              subtitle: 'demo@gmail.com',
+            ),
           ),
-          DrawerItemsListView()
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
+          ),
+          DrawerItemsListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(
+                  child: SizedBox(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: InActiveDrawerItem(
+                    drawerItemModel: DrawerItemModel(
+                      title: 'Setting system',
+                      image: Assets.imagesSettingIcon,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: InActiveDrawerItem(
+                    drawerItemModel: DrawerItemModel(
+                      title: 'Logout account',
+                      image: Assets.imagesLogoutIcon,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 48,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
--- lib/widgets/drawer_items_list_view.dart
+++ lib/widgets/drawer_items_list_view.dart
@@ -41,9 +41,9 @@ class _DrawerItemsListViewState extends State<DrawerItemsListView> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
+    return SliverList.builder(
+      // shrinkWrap: true,
+      // physics: const NeverScrollableScrollPhysics(),
       itemCount: items.length,
       itemBuilder: (context, index) {
         return InkWell(
