b4564e52cc72259f5f25a2da8edaf828c0dd33a6
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-05-20 23:00:17 +0200

    Add Drawer in mobile layout and fix some errors

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout_builder.dart';
+import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
+import 'package:responsive_dash_board/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dash_board/widgets/dashboard_tablet_layout.dart';
 
-class DashBoardView extends StatelessWidget {
+class DashBoardView extends StatefulWidget {
   const DashBoardView({super.key});
+
+  @override
+  State<DashBoardView> createState() => _DashBoardViewState();
+}
+
+class _DashBoardViewState extends State<DashBoardView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      appBar: MediaQuery.sizeOf(context).width < 800
+          ? AppBar(
+              elevation: 0,
+              backgroundColor: const Color(0xffFAFAFA),
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.menu_rounded,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  scaffoldKey.currentState!.openDrawer();
+                },
+              ),
+            )
+          : null,
+      drawer: MediaQuery.sizeOf(context).width < 800
+          ? SizedBox(
+              width: MediaQuery.sizeOf(context).width * .6,
+              child: const CustomDrawer())
+          : null,
       backgroundColor: const Color(0xffF7F9FA),
       body: AdaptiveLayoutBuilder(
-        mobileLayout: (context) => const SizedBox(),
+        mobileLayout: (context) => const DashboardMobileLayout(),
         tabletLayout: (context) => const DashboardTabletLayout(),
         desktopLayout: (context) => const DashboardDesktopLayout(),
       ),
